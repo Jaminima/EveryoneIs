dee55b1aca05b5fc88e05e8b68ad645e9c790b97
Oscar Davies <oscar.william.davies@outlook.com>
2021-04-01 19:52:08 +0100

    Changing Greeting

---
+++ EveryoneIs/wwwroot/js/type_async.js
@@ -0,0 +1,56 @@
+﻿async function init() {
+    const node = document.querySelector("#type-text")
+
+    await sleep(1000)
+    await node.type("")
+
+    let names = [
+        "John","James","Mark","Eclaire","Adolf","Infected"
+    ];
+    let n;
+
+    while (true) {
+        n = names[Math.floor(Math.random() * names.length)];
+        await node.type(n);
+        await sleep(5000)
+        await node.delete(n);
+    }
+}
+
+
+// Source code 🚩
+
+const sleep = time => new Promise(resolve => setTimeout(resolve, time))
+
+class TypeAsync extends HTMLSpanElement {
+    get text() {
+        return this.innerText
+    }
+    set text(value) {
+        return this.innerHTML = value
+    }
+
+    get typeInterval() {
+        const randomMs = 300 * Math.random()
+        return randomMs < 50 ? 10 : randomMs
+    }
+
+    async type(text) {
+        for (let character of text) {
+            this.text += character
+            await sleep(this.typeInterval)
+        }
+    }
+
+    async delete(text) {
+        for (let character of text) {
+            this.text = this.text.slice(0, this.text.length - 1)
+            await sleep(this.typeInterval)
+        }
+    }
+}
+
+customElements.define('type-async', TypeAsync, { extends: 'span' })
+
+
+init()
